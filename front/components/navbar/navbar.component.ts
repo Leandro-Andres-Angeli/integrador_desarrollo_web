fc7d5c6d17cf27a34f3f2116bb0fee992ea9e94b
Leandro Angeli <leandroandresangeli@gmail.com>
2025-04-07 22:30:49 -0300

    'bootstrap'

---
+++ front/components/navbar/navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+@Component({
+  selector: 'app-navbar',
+  imports: [NgbDropdownModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent {
+
+}
--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from '../../views/home/home.component';
+import { CreateSurveyComponent } from '../../views/create-survey/create-survey.component';
+import { NotFound404Component } from '../../views/not-found404/not-found404.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path:"" , 
+    pathMatch:'full',
+    component:HomeComponent
+} , {
+    path:"encuesta",
+    component:CreateSurveyComponent
+    
+},{
+    path:"**",
+    component:NotFound404Component
+}];
