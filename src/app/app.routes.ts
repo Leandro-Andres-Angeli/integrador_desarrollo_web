3d3adb744c5e2a427d243e4574066f264b825da0
Leandro Angeli <leandroandresangeli@gmail.com>
2025-04-27 19:39:08 -0300

    before refactor

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,24 +1,14 @@
 import { Routes } from '@angular/router';
 import { EncuestaComponent } from './components/encuesta/encuesta.component';
 import { CreateSurveyComponent } from './views/create-survey/create-survey.component';
+import { HomeComponent } from './views/home/home.component';
 
 export const routes: Routes = [
   { 
     path: '', 
-    redirectTo: '/encuestas', 
-    pathMatch: 'full' 
+    
+    pathMatch: 'full', 
+    component : HomeComponent
   },
-  { 
-    path: 'encuestas', 
-    component: EncuestaComponent 
-  },
-  { 
-    path: 'responder/:id', 
-    component: EncuestaComponent 
-  }
-  ,
-  { 
-    path: 'crear-encuesta', 
-    component: CreateSurveyComponent
-  }
+  
 ];
