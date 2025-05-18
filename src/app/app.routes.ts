6e46aa5a3832dd9549d389c24255d6b0b8b7dccc
Leandro Andres Angeli <leandroandresangeli@gmail.com>
2025-05-18 18:58:54 -0300

    fixed export home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 
-import { HomeComponent } from './views/home/home.component';
 import { CrearEncuestaComponent } from './components/crearEncuesta/creaEncuesta.component';
 import { HomeComponent } from './views/home/home.component';
 
