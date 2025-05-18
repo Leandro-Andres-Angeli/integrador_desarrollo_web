e3588f0f260c0de23e087bbf8344d28be7110126
Leandro Andres Angeli <leandroandresangeli@gmail.com>
2025-05-18 18:56:31 -0300

    fixed export home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,19 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
+
 import { CrearEncuestaComponent } from './components/crearEncuesta/creaEncuesta.component';
+import { HomeComponent } from './views/home/home.component';
 
 export const routes: Routes = [
-    {
-        path: '', 
-            component: HomeComponent,
-    },
-    {
-        path: 'crearEncuesta',
-            component: CrearEncuestaComponent,
-    },
-    {
-        path: '**', 
-            redirectTo: ''     
-    }
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'crearEncuesta',
+    component: CrearEncuestaComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
 ];
