31aba9723feef5c4cfff5c2d50dbf2f4e475eb42
Nahir-Orellana-Lopez <nahir.orellana.lopez@gmail.com>
2025-05-28 17:08:21 -0300

    fix archivo que quedó por fuera del merge

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { CrearEncuestaComponent } from './components/crearEncuesta/creaEncuesta.component';
 import { HomeComponent } from './views/home/home.component';
 import { Error404Component } from './components/Errores/error404/error404.component';
+import { AgradecimientoComponent } from './components/agradecimiento/agradecimiento.component';
 import { ResultadosComponent } from './views/resultados/resultados.component';
 
 export const routes: Routes = [
@@ -20,6 +21,10 @@ export const routes: Routes = [
   {
     path: 'resultados/:id',
     component: ResultadosComponent,
+  },
+  {
+    path: 'agradecimiento',
+    component: AgradecimientoComponent,
   },
   {
     path: '**',
