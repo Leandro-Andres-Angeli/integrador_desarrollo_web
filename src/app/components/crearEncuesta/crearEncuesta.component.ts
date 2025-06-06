a3dbda116eef6a2f89d50b37d454bac67df1974e
Leandro Andres Angeli <leandroandresangeli@gmail.com>
2025-06-06 19:40:50 -0300

    fixed validation

--- src/app/components/crearEncuesta/crearEncuesta.component.ts
+++ src/app/components/crearEncuesta/crearEncuesta.component.ts
@@ -110,7 +110,7 @@ export class CrearEncuestaComponent {
     this.preguntas.push(pregunta);
   }
   handleTipoRespuestaChange(pregunta: FormGroup) {
-    if (pregunta.controls['tipo'].value === TiposRespuestaEnum.ABIERTA) {
+    if (pregunta.controls['tipo'].value !== TiposRespuestaEnum.OPCION_MULTIPLE_SELECCION_MULTIPLE && pregunta.controls['tipo'].value !== TiposRespuestaEnum.OPCION_MULTIPLE_SELECCION_SIMPLE) {
       pregunta.removeControl('opciones');
     } else {
       console.log('here');
@@ -175,17 +175,17 @@ export class CrearEncuestaComponent {
       tipo: p.get('tipo')?.value,
       opciones: (
         p.get('tipo')?.value ===
-          TiposRespuestaEnum.OPCION_MULTIPLE_SELECCION_SIMPLE ||
+        TiposRespuestaEnum.OPCION_MULTIPLE_SELECCION_SIMPLE ||
         p.get('tipo')?.value ===
-          TiposRespuestaEnum.OPCION_MULTIPLE_SELECCION_MULTIPLE ||
+        TiposRespuestaEnum.OPCION_MULTIPLE_SELECCION_MULTIPLE ||
         p.get('tipo')?.value ===
-          TiposRespuestaEnum.VERDADERO_FALSO
+        TiposRespuestaEnum.VERDADERO_FALSO
       )
-          ? this.getOpciones(p).controls.map((ctrl, idx) => ({
-              texto: ctrl.value,
-              numero: idx + 1,
-            }))
-          : [],
+        ? this.getOpciones(p).controls.map((ctrl, idx) => ({
+          texto: ctrl.value,
+          numero: idx + 1,
+        }))
+        : [],
     }));
 
     this.encuestasService
